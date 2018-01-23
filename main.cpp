246e175ceba49f836fa4b10c6f6d08b0eb40825a
CaoDaQian <1036415692@qq.com>
2018-01-23 16:29:52 +0800

    client框架构图中
    2018年1月23日

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
+#include<cstdlib>
+#include"tcpclient.h"
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-    
+	if(argc < 3)
+	{
+		cerr << "arg is not enough! (ip,port)" << endl;
+	}
 
+	char *ip = argv[1];
+	int port = atoi(argv[2]);
+
+	tcpclient cli(ip, port);
+
+	cli.run();
 
     return 0;
 }
---
+++ tcpclient.h
@@ -0,0 +1,22 @@
+#pragma once
+#pragma execution_character_set("utf-8")
+#ifndef TCPCLIENT_H
+#define TCPCLIENT
+
+#include <string>
+using namespace std;
+
+typedef class Tcpclient
+{
+public:
+	Tcpclient(char *ip, int port);
+	~Tcpclient();
+	void run();
+private:
+	string _cmd;
+	int _sockfd;
+}tcpclient,*pTcpclient;
+
+
+
+#endif // !TCPCLIENT_H
