fc9f85c5b75f3263db60a21c886fd6baeb9ab7cf
CDQ <1036415692@qq.com>
2018-01-23 15:05:30 +0800

    client

--- view_exit.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "view_exit.h"
-#include <mysql/mysql.h>
-using namespace std;
-
-view_exit::view_exit(void *mpcon)
-{
-    _mpcon = (MYSQL *)mpcon;
-}
--- view_talk_one.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "view_talk_one.h"
-#include <mysql/mysql.h>
-using namespace std;
-
-view_talk_one::view_talk_one(void *mpcon)
-{
-    _mpcon = (MYSQL *)mpcon;
-}
