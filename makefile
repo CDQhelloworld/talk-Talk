80608a6c1f559fb935c054898b6b38fde7a44742
CDQ <1036415692@qq.com>
2018-01-24 11:04:56 +0800

    客户端测试雏形

--- makefile
+++ makefile
@@ -1,22 +1,10 @@
-server_main : main.o tcpserver.o mpthread.o control.o view_login.o view_register.o view_exit.o view_talk_one.o
-	g++ -o server_main main.o tcpserver.o mpthread.o control.o view_login.o view_register.o view_exit.o view_talk_one.o -levent -ljson -lpthread -lmysqlclient
+cli_main : main.o tcpclient.o
+	gcc -o cli_main main.o tcpclient.o -ljson
 
-main.o : main.cpp
-	g++ -c main.cpp
-tcpserver.o : tcpserver.cpp
-	g++ -c tcpserver.cpp -levent
-mpthread.o : mpthread.cpp
-	g++ -c mpthread.cpp -levent
-control.o : control.cpp view_login.cpp view_exit.cpp view_talk_one.cpp view_register.cpp
-	g++ -c control.cpp view_login.cpp view_exit.cpp view_talk_one.cpp view_register.cpp
-view_login.o : view_login.cpp
-	g++ -c view_login.cpp
-view_register.o : view_register.cpp
-	g++ -c view_register.cpp
-view_exit.o : view_exit.cpp 
-	g++ -c view_exit.cpp
-view_talk_one.o : view_talk_one.cpp 
-	g++ -c view_talk_one.cpp
+main.o : main.cpp tcpclient.cpp
+	gcc -c main.cpp tcpclient.cpp -ljson
+tcpclient.o : tcpclient.cpp
+	gcc -c tcpclient.cpp -ljson
 
 clean :
-	rm server_main main.o tcpserver.o mpthread.o control.o view_login.o
+	rm -f *.o cli_main
--- tcpclient.cpp
+++ tcpclient.cpp
@@ -1,56 +1,98 @@
 #pragma execution_character_set("utf-8") 
-#include<iostream> 
-#include<errno.h> 
-#include<sys/socket.h> 
-#include<memory> 
+#include<iostream>
+#include<errno.h>
+#include<sys/socket.h>
+#include<memory>
 #include<cstdlib>
-#include"tcpclient.h" 
-using namespace std;  
+#include"tcpclient.h"
+#include"public.h"
+#include<arpa/inet.h>
+#include<string.h>
+#include<memory>
+#include<json/json.h>
+using namespace std;
 
-Tcpclient::Tcpclient(char *ip, int port) 
-{ 	
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0); 	
-        if(sockfd == -1) 	
-        { 		
-            cerr << "socket fail; errno:" << errno << endl; 	
-        }  	
+Tcpclient::Tcpclient(char *ip, int port)
+{
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if(sockfd == -1)
+        {
+            cerr << "socket fail; errno:" << errno << endl;
+        }
 
-        _sockfd = sockfd; 	
-        struct sockaddr_in saddr; 	
-        memset(&(saddr), 0, sizeof(saddr)); 	
-        saddr.sin_family = AF_INET; 	
-        saddr.sin_port = htons(port); 	
-        saddr.sin_addr.s_addr = inet_addr(ip); 	
-        int res = connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr)); 	
-        if(res == -1) 	
-        { 		
-            cerr << "connect fail! errno:" << errno << endl; 		
-            exit(1); 	
-        } 
-}  
-
-Tcpclient::~Tcpclient() { }  
-
-void Tcpclient::run() 
-{ 	char cmd[1024];
-    while(1) 	
-    { 		
-        memset(cmd, 0, strlen(cmd)); 		
-        cin >> cmd;
+        _sockfd = sockfd;
+        struct sockaddr_in saddr;
+        memset(&(saddr), 0, sizeof(saddr));
         
+        saddr.sin_family = AF_INET;
+        saddr.sin_port = htons(port);
+        saddr.sin_addr.s_addr = inet_addr(ip);
+        int res = connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr));
+        if(res == -1)
+        {
+            cerr << "connect fail! errno:" << errno << endl;
+            exit(1);
+        }
+}
+
+Tcpclient::~Tcpclient(){}
+
+static void Show(const char *buff)
+{
+    for(int i = 0; i < strlen(buff); ++i)
+    {
+        cout<<buff[i];
+    }
+}
+
+void Tcpclient::run()
+{
+    char cmd[1024];
+    while(1)
+    {
+        memset(cmd, 0, strlen(cmd));	
+        cin >> cmd;
+
         if(strcmp(cmd, "login"))
         {
+            Json::Value val;
+            val["type"] = MSG_TYPE_LOGIN;
             cout<<"Input your name:";
             char buff[1024] = {0};
             cin>>buff;
-            Json::Value val;
             val["name"] = buff;
             cout<<"Input your password:";
             memset(buff, 0, strlen(buff));
             cin>>buff;
             val["pw"] = buff; 
             
-            send(sockfd, val.toStyledString().c_str(), strlen(val.toStyledString().c_str()), 0);
+            send(_sockfd, val.toStyledString().c_str(), strlen(val.toStyledString().c_str()), 0);
+            char recvBuff[2048];
+            while(0 < (recv(_sockfd, recvBuff, sizeof(recvBuff)/sizeof(recvBuff[0]), 0)))
+            {
+                Show(recvBuff);
+            }
+
         }
-    } 
+        else if(strcmp(cmd, "rigister"))
+        {
+            Json::Value val;
+            val["type"] = MSG_TYPE_REGISTER;
+            cout<<"Input your name:";
+            char buff[1024] = {0};
+            cin>>buff;
+            val["name"] = buff;
+            cout<<"Input your password:";
+            memset(buff, 0, strlen(buff));
+            cin>>buff;
+            val["pw"] = buff; 
+
+            send(_sockfd, val.toStyledString().c_str(), strlen(val.toStyledString().c_str()), 0);
+            char recvBuff[2048];
+            while(0 < (recv(_sockfd, recvBuff, sizeof(recvBuff)/sizeof(recvBuff[0]), 0)))
+            {
+                Show(recvBuff);
+            }
+        }
+    }
 }
